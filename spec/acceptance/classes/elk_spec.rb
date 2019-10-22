5267e68d9078986e21de3ae52a4188355cc91ac7
Mathias Hansson <drokkie@gmail.com>
2019-10-22 07:43:33 -0700

    linting done?

--- spec/acceptance/classes/elk_spec.rb
+++ spec/acceptance/classes/elk_spec.rb
@@ -2,12 +2,11 @@ require 'spec_helper_acceptance'
 
 describe 'elk class' do
   context 'default parameters' do
-    let(:manifest) 
-    { 
-        "class {'elk':}"}
-    it 'should run without errors' do
-        result = apply_manifest(manifest, :catch_failures => true)
-        expect(result.exit_code).to eq 2
+    let(:manifest) { "class {'elk':}" }
+
+    it 'will run without errors' do
+      result = apply_manifest(manifest, catch_failures: true)
+      expect(result.exit_code).to eq 2
     end
   end
 end
