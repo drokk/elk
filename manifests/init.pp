421dde73c540a30ad954e0a27562fe654a163f76
Mathias Hansson <drokkie@gmail.com>
2019-10-26 07:32:39 -0700

    java ...

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,7 @@ class elk {
 
   include ::java
   class { 'elasticsearch':
-    jvm_options => ['-Xms256m','-Xmx256m'8:-XX:NumberOfGCLogFiles=32','8:-XX:GCLogFileSize=64m','8:-XX:+UseGCLogFileRotation','8:-XX:+PrintTenuringDistribution','8:-XX:+PrintGCDateStamps','8:-XX:+PrintGCApplicationStoppedTime','8:-XX:+UseConcMarkSweepGC','8:-XX:+UseCMSInitiatingOccupancyOnly','11:-XX:+UseG1GC','11:-XX:InitiatingHeapOccupancyPercent=75']
+    jvm_options => ['-Xms256m','-Xmx256m','8:-XX:NumberOfGCLogFiles=32','8:-XX:GCLogFileSize=64m','8:-XX:+UseGCLogFileRotation','8:-XX:+PrintTenuringDistribution','8:-XX:+PrintGCDateStamps','8:-XX:+PrintGCApplicationStoppedTime','8:-XX:+UseConcMarkSweepGC','8:-XX:+UseCMSInitiatingOccupancyOnly','11:-XX:+UseG1GC','11:-XX:InitiatingHeapOccupancyPercent=75']
   }
   elasticsearch::instance { 'es-01': }
 
